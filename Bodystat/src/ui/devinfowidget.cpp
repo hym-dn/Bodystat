15130c768f1e6c0cdba960aaa0ed9dbf79f247a4
hym_dn <yameng_he@aliyun.com>
2018-07-10 22:49:20 +0800

    完成设备信息界面搭建。

--- Bodystat/src/ui/devinfowidget.cpp
+++ Bodystat/src/ui/devinfowidget.cpp
@@ -1,5 +1,6 @@
 ﻿#include"devinfowidget.h"
 #include"ui_devinfowidget.h"
+#include<QMovie>
 
 DevInfoWidget::DevInfoWidget(QWidget *parent/*=0*/)
     :MdiSubWidget(parent)
@@ -15,4 +16,11 @@ DevInfoWidget::~DevInfoWidget(){
 void DevInfoWidget::initUi(){
     _ui->_bluetoothLabel->setPixmap(
         QPixmap(":rc/bluetoothdisabled.png"));
+    _ui->_devLabel->setPixmap(
+        QPixmap(":rc/Quadscan4000.bmp"));
+    QMovie *movie=new QMovie(":rc/progress.gif",
+        QByteArray(),_ui->_progressLabel);
+    _ui->_progressLabel->setMovie(movie);
+    movie->start();
+    //_ui->_progressLabel->hide();
 }
